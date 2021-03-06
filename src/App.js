1600317b91768849b66575fd672dc3857104fed9
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-03-06 21:07:31 +0800

    edit home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Nav from "./components/nav";
 import Pricing from "./components/Pricing";
+import Penjelas from "./components/Penjelas";
+import Keunggulan from "./components/Keunggulan";
 
 const App = () => {
   return (
     <>
-     <Nav />
+      <Nav />
       <Hero />
+      <Penjelas />
+      <Keunggulan />
       <Pricing />
       <Footer />
       <Switch></Switch>
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -14,7 +14,7 @@ const Pricing = () => {
           <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
             <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
               <div className="p-8 text-3xl font-bold text-center border-b-4">
-                Free
+                Dasar
               </div>
               <ul className="w-full text-center text-sm">
                 <li className="border-b py-4">Thing</li>
@@ -24,12 +24,12 @@ const Pricing = () => {
             </div>
             <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
               <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                £0
-                <span className="text-base">for one user</span>
+                RP. 130.000
+                <span className="text-base">/persemester</span>
               </div>
               <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
+                <button className="mx-auto lg:mx-0 hover:underline border border-gray-100 text-gray-600 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Daftar
                 </button>
               </div>
             </div>
@@ -37,7 +37,7 @@ const Pricing = () => {
           <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
             <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
               <div className="w-full p-8 text-3xl font-bold text-center">
-                Basic
+                Menengah
               </div>
               <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
               <ul className="w-full text-center text-base font-bold">
@@ -49,12 +49,12 @@ const Pricing = () => {
             </div>
             <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
               <div className="w-full pt-6 text-4xl font-bold text-center">
-                £x.99
-                <span className="text-base">/ per user</span>
+              RP. 250.000
+                <span className="text-base">/persemester</span>
               </div>
               <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
+                <button className="mx-auto lg:mx-0 hover:underline border border-gray-100 text-gray-600font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Daftar
                 </button>
               </div>
             </div>
@@ -62,7 +62,7 @@ const Pricing = () => {
           <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
             <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
               <div className="p-8 text-3xl font-bold text-center border-b-4">
-                Pro
+               Tinggi
               </div>
               <ul className="w-full text-center text-sm">
                 <li className="border-b py-4">Thing</li>
@@ -72,12 +72,12 @@ const Pricing = () => {
             </div>
             <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
               <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                £x.99
-                <span className="text-base">/ per user</span>
+              RP. 500.000
+                <span className="text-base">/persemester</span>
               </div>
               <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
+                <button className="mx-auto lg:mx-0 border border-gray-100 text-gray-600 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  Daftar
                 </button>
               </div>
             </div>
