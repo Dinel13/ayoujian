c69a08653bbbdfe1a0f741eef9f85e2e25497c57
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-03-07 17:32:52 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  
+    next();
+  });
+
+  app.use((error, req, res, next) => {
+    if (req.file) {
+      fs.unlink(req.file.path, err => {
+        console.log(err);
+      });
+    }
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+  });
+
+  app.listen(8080)
+  
